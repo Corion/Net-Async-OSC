d9c203b8a1d4c5dbb02ecd63d2e0f028fbed8098
Max Maischein <corion@corion.net>
2023-04-21 14:17:14 +0200

    We can spew sound now
    ... and even have an idea on how to extend this.
    
    Still need to split up the synthesizer/parameter so we can
    have effects active. Maybe this needs support on the
    receiving side as well?!

--- scripts/send-osc.pl
+++ scripts/send-osc.pl
@@ -14,7 +14,7 @@ my $osc = Protocol::OSC->new;
 sub new_socket( $loop, $host, $port ) {
     my $pingback = IO::Async::Socket->new(
         on_recv => sub( $sock, $data, $addr, @rest ) {
-			warn "Reply: $data";
+            warn "Reply: $data";
         },
         #on_recv_error => sub {
         #   my ( $self, $errno ) = @_;
@@ -28,31 +28,82 @@ sub new_socket( $loop, $host, $port ) {
         service => $port,
         socktype => 'dgram'
     )->get;
-	return $pingback
+    return $pingback
 }
 
 sub send_osc( $socket, @message ) {
-	my $data = $osc->message(@message); # pack
-	say join " , " , @{ $osc->parse( $data ) };
-	$socket->send( $data );
+    my $data = $osc->message(@message); # pack
+    send_osc_msg( $socket, $data );
+}
+
+sub send_osc_msg( $socket, $data ) {
+    say join " , " , @{ $osc->parse( $data ) };
+    $socket->send( $data );
 }
 
 my $loop = IO::Async::Loop->new();
 my $udp = new_socket($loop, '127.0.0.1', 4560);
 
 my $bpm = 120;
+my $beats = 4; # 4/4
+my $ticks = 4; # ticks per beat
+my $tracks = 4; # so far...
 
-my $sequencer = [
-];
+sub loc($tick, $track) {
+    $tick*$tracks+$track
+}
+
+sub beat($beat, $track) {
+    return loc($ticks*$beat,$track)
+}
+
+my $sequencer = [];
+
+for my $beat (0..7) {
+    $sequencer->[beat($beat,0)] = [
+    # Maybe we should pre-cook the OSC message even, to take
+    # load out of the output loop
+    "/trigger/tb303" => 'ififi',
+        (70+int(rand(12)), 0.125, 60+int(rand(50)), 0.8, 0)
+    ];
+}
+my $last = beat(8,0)-1;
+$sequencer->[$last]= undef;
+use Data::Dumper; warn Dumper $sequencer;
+
+my $tick = 0;
+my $ticks_in_bar = @$sequencer / $tracks / $beats;
+say "You have defined $ticks_in_bar ticks";
 
 my $timer = IO::Async::Timer::Periodic->new(
-	reschedule => 'skip',
-	first_interval => 0,
-	interval => 60/$bpm,
-	on_tick => sub {
-		my ($note, $release, $cutoff, $res, $wave) = (70+int(rand(12)), 0.125, 60+int(rand(50)), 0.8, 0);
-		send_osc( $udp, "/trigger/tb303" => 'ififi', $note, $release, $cutoff, $res, $wave );
-	},
+    reschedule => 'skip',
+    first_interval => 0,
+    interval => 60/$bpm/$beats/$ticks,
+    on_tick => sub {
+        my $loc = loc($tick, 0) % @$sequencer;
+
+        # Consider calculating the tick from the start of the
+        # playtime instead of blindly increasing it?!
+        $tick = ($tick+1)%$ticks_in_bar;
+        say sprintf '%d / %d / %d', $tick, $loc, scalar @$sequencer;
+        for my $s ($loc..$loc+$tracks-1) {
+            my $n = $sequencer->[$s];
+            if( $n ) {
+                my $r = ref $n;
+                if( $r ) {
+                    if( $r eq 'CODE' ) {
+                        # Can we pass any meaningful parameters here?
+                        # Like maybe the current tick?!
+                        send_osc( $udp, $n->($tick) );
+                    } elsif( $r eq 'ARRAY' ) {
+                        send_osc( $udp, @$n );
+                    }
+                } else {
+                    send_msg( $udp, $n );
+                }
+            }
+        }
+    },
 );
 
 # send_osc "/loader/require", "tempfile" ?!

f5ff5c25726ad195b0b0918c4ff840d28c2a15a2
Max Maischein <corion@corion.net>
2023-04-23 16:12:26 +0200

    Add a 4/4 drum
    ... next up is finding some harmonies to make the (random)
    acid arpeggios more accessible

--- scripts/send-osc.pl
+++ scripts/send-osc.pl
@@ -46,7 +46,7 @@ my $udp = new_socket($loop, '127.0.0.1', 4560);
 
 my $bpm = 120;
 my $beats = 4; # 4/4
-my $ticks = 4; # ticks per beat
+my $ticks = 4; # ticks per beat, means 1/16th notes
 my $tracks = 4; # so far...
 
 sub loc($tick, $track) {
@@ -59,20 +59,25 @@ sub beat($beat, $track) {
 
 my $sequencer = [];
 
-for my $beat (0..7) {
-    $sequencer->[beat($beat,0)] = [
+for my $beat (0..15) {
+    $sequencer->[beat($beat*2,0)] = [
     # Maybe we should pre-cook the OSC message even, to take
     # load out of the output loop
-    "/trigger/tb303" => 'ififi',
-        (70+int(rand(12)), 0.125, 60+int(rand(50)), 0.8, 0)
+    "/trigger/tb303" => 'iiffi',
+        #(70+int(rand(12)), 0.125, 60+int(rand(50)), 0.8, 0)
+        (40+int(rand(24)), 130, 0.1, 0.8+rand(0.15), 0)
     ];
+	# 4/4 bassdrum
+    $sequencer->[beat($beat*4,1)] = 
+        $osc->message("/trigger/bd", 'i' => 5);
+    ;
 }
-my $last = beat(8,0)-1;
+my $last = beat(16,0)-1;
 $sequencer->[$last]= undef;
 use Data::Dumper; warn Dumper $sequencer;
 
 my $tick = 0;
-my $ticks_in_bar = @$sequencer / $tracks / $beats;
+my $ticks_in_bar = @$sequencer / $tracks;
 say "You have defined $ticks_in_bar ticks";
 
 $| = 1;
@@ -81,30 +86,38 @@ $| = 1;
 
 # We should be able to pause / restart / resume / resync the code
 # For resync, we need to keep track of the start time or increase our tick counter
+# Currently we simply increase our tick counter while we are silent. This means
+# we have no real "pause". We need to store two tick states.
 
-my $play_state = 'paused';
+my $output_state = '';
 
 sub play_sounds {
 	my $loc = loc($tick, 0) % @$sequencer;
-
+	
 	# Consider calculating the tick from the start of the
 	# playtime instead of blindly increasing it?!
 	$tick = ($tick+1)%$ticks_in_bar;
-	print sprintf "%d / %d / %d\r", $tick, $loc, scalar @$sequencer;
-	for my $s ($loc..$loc+$tracks-1) {
-		my $n = $sequencer->[$s];
-		if( $n ) {
-			my $r = ref $n;
-			if( $r ) {
-				if( $r eq 'CODE' ) {
-					# Can we pass any meaningful parameters here?
-					# Like maybe the current tick?!
-					send_osc( $udp, $n->($tick) );
-				} elsif( $r eq 'ARRAY' ) {
-					send_osc( $udp, @$n );
+	
+	if( $output_state eq 'silent' ) {
+		# do nothing
+	} else {
+		print sprintf "%d / %d / %d\r", $tick, $loc, scalar @$sequencer;
+		for my $s ($loc..$loc+$tracks-1) {
+			my $n = $sequencer->[$s];
+			if( $n ) {
+				my $r = ref $n;
+				if( $r ) {
+					if( $r eq 'CODE' ) {
+						# Can we pass any meaningful parameters here?
+						# Like maybe the current tick?!
+						send_osc( $udp, $n->($tick) );
+					} elsif( $r eq 'ARRAY' ) {
+						send_osc( $udp, @$n );
+					}
+					
+				} else {
+					send_osc_msg( $udp, $n );
 				}
-			} else {
-				send_msg( $udp, $n );
 			}
 		}
 	}

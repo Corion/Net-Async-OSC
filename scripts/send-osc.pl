366549bead28252dec11152b30a9ee8ba09492d3
Max Maischein <corion@corion.net>
2023-05-19 14:32:56 +0200

    Parse drum patterns from pattern strings
    ... we likely only want the melody (?) to be autogenerated, not the
    drum beats

--- scripts/send-osc.pl
+++ scripts/send-osc.pl
@@ -2,6 +2,7 @@
 use 5.020;
 use feature 'signatures';
 no warnings 'experimental::signatures';
+use Carp 'croak';
 
 use Protocol::OSC;
 use IO::Async::Loop;
@@ -59,25 +60,69 @@ sub beat($beat, $track) {
 
 my $sequencer = [];
 
+# We don't only want full programmatic stuff, we also want
+# to specify stuff via patterns, just like in any sequencer.
+# Most likely we want to parse (drum) patterns like:
+#
+# HH|x-x-x-x-x-x-x-x-||
+#  S|--------o-------||
+#  B|o---------------||
+#    1 + 2 + 3 + 4 +
+
 for my $beat (0..15) {
-    $sequencer->[beat($beat*2,0)] = [
+    $sequencer->[beat($beat*4+2,0)] = [
     # Maybe we should pre-cook the OSC message even, to take
     # load out of the output loop
     "/trigger/tb303" => 'iiffi',
         #(70+int(rand(12)), 0.125, 60+int(rand(50)), 0.8, 0)
         (40+int(rand(24)), 130, 0.1, 0.8+rand(0.15), 0)
     ];
-	# 4/4 bassdrum
-    $sequencer->[beat($beat*4,1)] = 
-        $osc->message("/trigger/bd", 'i' => 5);
-    ;
+	# 1/4 bassdrum
+	#if( $beat % 4 == 0 ) {
+	#	$sequencer->[beat($beat*4,1)] =
+	#		$osc->message("/trigger/bd", 'i' => 5);
+	#	;
+	#}
+	## 1/8 hi-hat
+    #$sequencer->[beat($beat*4,2)] =
+    #    $osc->message("/trigger/hh", 'i' => 1);
+    #;
+    #$sequencer->[beat($beat*4+2,2)] =
+    #    $osc->message("/trigger/hh", 'i' => 1);
+    #;
 }
-my $last = beat(16,0)-1;
+
+# we expect each char to be a 16th note
+sub parse_drum_pattern( $sequencer, $track, $pattern, $osc_message ) {
+	$pattern =~ m!^\s*\w+\s*\|((?:[\w\-]{16})+)\|+!
+	    or croak "Invalid pattern '$pattern'";
+	my @beats = split //, $1;
+	my $ofs = 0;
+
+	while( $ofs < @beats ) {
+		if( $beats[ $ofs ] ne '-' ) {
+			$sequencer->[beat($ofs,$track)] =
+			    $osc->message($osc_message, 'f' => 0.75);
+			;
+		}
+		$ofs++;
+	}
+}
+
+# 64 16th notes
+#                                     1   2   3   4   1   2   3   4
+parse_drum_pattern($sequencer, 2, 'HH|x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-||', '/trigger/hh');
+parse_drum_pattern($sequencer, 3, ' S|--------o---------------o---------------o---------------o-------||', '/trigger/sn');
+parse_drum_pattern($sequencer, 3, ' B|o---------------o---------------o---------------o---------------||', '/trigger/bd');
+
+# "Expand" the array to the full length
+my $last = beat(16*4,0)-1;
 $sequencer->[$last]= undef;
-use Data::Dumper; warn Dumper $sequencer;
 
 my $tick = 0;
 my $ticks_in_bar = @$sequencer / $tracks;
+
+die "data structure is not a complete bar ($ticks_in_bar)" if int($ticks_in_bar) != $ticks_in_bar;
 say "You have defined $ticks_in_bar ticks";
 
 $| = 1;
@@ -93,11 +138,7 @@ my $output_state = '';
 
 sub play_sounds {
 	my $loc = loc($tick, 0) % @$sequencer;
-	
-	# Consider calculating the tick from the start of the
-	# playtime instead of blindly increasing it?!
-	$tick = ($tick+1)%$ticks_in_bar;
-	
+
 	if( $output_state eq 'silent' ) {
 		# do nothing
 	} else {
@@ -114,13 +155,18 @@ sub play_sounds {
 					} elsif( $r eq 'ARRAY' ) {
 						send_osc( $udp, @$n );
 					}
-					
+
 				} else {
+		print sprintf "%d / %d / %d - $ticks_in_bar - beat\r\n", $tick, $loc, scalar @$sequencer;
 					send_osc_msg( $udp, $n );
 				}
 			}
 		}
 	}
+	# Consider calculating the tick from the start of the
+	# playtime instead of blindly increasing it?!
+	$tick = ($tick+1)%$ticks_in_bar;
+
 }
 
 
